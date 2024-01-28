48d80bfe67a50bb61783e9e6506c48fe33391400
AlexeyGold2077 <alexgold2077@gmail.com>
2024-01-28 17:00:17 +0300

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 echo "compiling..."
 javac --release 17 src/Server/Server.java src/Server/ClientHandler.java
+javac --release 17 src/Client/Client.java
+
 echo "archiving..."
 jar -cfm src/Server/ServerApp.jar src/Server/MANIFEST.mf src/Server/Server.class src/Server/ClientHandler.class
+jar -cfm src/Client/ClientApp.jar src/Client/MANIFEST.mf src/Client/Client.class
+
 echo "done!"
 
