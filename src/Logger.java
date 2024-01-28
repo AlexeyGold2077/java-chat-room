6f7060925cbb15134aede286d96892224229dda9
AlexeyGold2077 <alexgold2077@gmail.com>
2024-01-28 22:32:02 +0300

    Added Logger class

---
+++ src/Logger.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+
+    public String getDateTime() {
+		LocalDateTime dateTime = LocalDateTime.now();
+        return String.valueOf(dateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss d-MM-uuuu")));
+    }
+
+    public String getTime() {
+        return String.valueOf(LocalTime.now());
+    }
+
+    public String getDate() {
+        return String.valueOf(LocalDate.now());
+    }
+}
