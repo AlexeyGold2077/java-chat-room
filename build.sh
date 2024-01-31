010880aa608cb50c491f81bc9720f6df61d091fb
AlexeyGold2077 <alexgold2077@gmail.com>
2024-01-31 21:58:19 +0300

    Fixed the build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "compiling..."
-javac --release 17 src/Server.java src/ClientHandler.java
+javac --release 17 src/Server.java src/ClientHandler.java src/Logger.java
 javac --release 17 src/Client.java
 
 echo "archiving..."
-jar -cfm jar/ServerApp.jar src/MANIFEST_Server.mf src/Server.class src/ClientHandler.class
+jar -cfm jar/ServerApp.jar src/MANIFEST_Server.mf src/Server.class src/ClientHandler.class src/Logger.class
 jar -cfm jar/ClientApp.jar src/MANIFEST_Client.mf src/Client.class
 
 echo "cleaning..."
