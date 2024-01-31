629fa3b8745d9068285030a7919b507c53d0e166
AlexeyGold2077 <alexgold2077@gmail.com>
2024-01-31 21:48:23 +0300

    Optimized the build script

--- build.sh
+++ build.sh
@@ -3,16 +3,10 @@ javac --release 17 src/Server.java src/ClientHandler.java
 javac --release 17 src/Client.java
 
 echo "archiving..."
-jar -cfm src/ServerApp.jar src/MANIFEST_Server.mf src/Server.class src/ClientHandler.class
-jar -cfm src/ClientApp.jar src/MANIFEST_Client.mf src/Client.class
-
-echo "moving..."
-rm -rf jar
-mkdir -p jar
-mv src/ServerApp.jar ./jar
-mv src/ClientApp.jar ./jar
+jar -cfm jar/ServerApp.jar src/MANIFEST_Server.mf src/Server.class src/ClientHandler.class
+jar -cfm jar/ClientApp.jar src/MANIFEST_Client.mf src/Client.class
 
 echo "cleaning..."
-rm -rf src/*.class rm src/*.jar
+rm -rf src/*.class
 
 echo "done!"
